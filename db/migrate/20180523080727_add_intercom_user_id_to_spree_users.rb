f2232ae29e46d3843a50af89db36676b94012fef
Ricardo Mazzini <mazzini@bit32.it>
2019-12-11 10:07:29 -0500

    Update 20180523080727_add_intercom_user_id_to_spree_users.rb

--- db/migrate/20180523080727_add_intercom_user_id_to_spree_users.rb
+++ db/migrate/20180523080727_add_intercom_user_id_to_spree_users.rb
@@ -1,4 +1,4 @@
-class AddIntercomUserIdToUsers < ActiveRecord::Migration[5.0]
+class AddIntercomUserIdToSpreeUsers < ActiveRecord::Migration[5.0]
   def change
     add_column Spree.user_class.table_name.to_sym, :intercom_user_id, :string
     add_index Spree.user_class.table_name.to_sym, :intercom_user_id, unique: true
